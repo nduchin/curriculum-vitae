66597e61e5e2f4dd36762443400ae687ee55e149
nduchin <nduchin@mail.ru>
2022-12-17 05:44:36 +0300

    feat(comp): complete project cards component

--- cv/lib/comp/component.js
+++ cv/lib/comp/component.js
@@ -1,7 +1,12 @@
-const list=document.getElementsByClassName("skill-component")
-console.log(list)
-Object.keys(list).forEach((k)=>{
-  let filename=list[k].innerHTML
+const list1=document.getElementsByClassName("skill-component")
+Object.keys(list1).forEach((k)=>{
+  let filename=list1[k].innerHTML
   console.log(filename)
-  list[k].innerHTML=`<object data="./lib/comp/skills_svg/${filename}" type="image/svg+xml"></object>`
+  list1[k].innerHTML=`<object data="./lib/comp/skills_svg/${filename}" type="image/svg+xml"></object>`
+})
+const list2=document.getElementsByClassName("project-component")
+Object.keys(list2).forEach((k)=>{
+  let filename=list2[k].innerHTML
+  console.log(filename)
+  list2[k].innerHTML=`<object data="./lib/comp/proj_svg/${filename}" type="image/svg+xml"></object>`
 })
